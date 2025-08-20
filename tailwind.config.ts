0603afaa68c65b9cafc085a52e537abf7a7a748b
jerryjlwang <jerryjwang@icloud.com>
2025-08-20 15:57:02 -0700

    Update tailwind.config.ts
    Added merriweather font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+        		merriweather: ["Merriweather", "serif"],
+      			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
